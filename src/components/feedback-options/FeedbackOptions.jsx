4da6256ea7dbba256a9e8779dd7488c6b8b8c2a5
Michael Kononenko <michael.kononenko101@gmail.com>
2022-12-09 00:29:26 +0200

    litle bit of decorating

--- src/components/feedback-options/FeedbackOptions.jsx
+++ src/components/feedback-options/FeedbackOptions.jsx
@@ -1,11 +1,11 @@
-import { OptionsWrapper } from "./FeedbackOptions.styled"
+import { FeedbackButton, OptionsWrapper } from "./FeedbackOptions.styled"
 
 export const FeedbackOptions = ({options, onLeaveFeedback}) => {
     const optionsArray = Object.keys(options);
     return (
         <OptionsWrapper style={{marginBottom: "20px"}}>
         {optionsArray.map(item => {
-            return(<button type="button" key={item} onClick={() => onLeaveFeedback(item)} style={{ padding:"5px", fontSize:"20px", borderRadius:"10px"}}>{item}</button>)})
+            return(<FeedbackButton type="button" key={item} onClick={() => onLeaveFeedback(item)} >{item}</FeedbackButton>)})
         }
         </OptionsWrapper>
     )
--- src/components/feedback-options/FeedbackOptions.styled.js
+++ src/components/feedback-options/FeedbackOptions.styled.js
@@ -5,3 +5,12 @@ display: flex;
 justify-content: space-between;
 width: 100%;
 `
+
+export const FeedbackButton = styled.button`
+padding: 5px; 
+font-size: 20px;
+border-radius: 10px;
+:active{
+    background-color: gray;
+}
+`
