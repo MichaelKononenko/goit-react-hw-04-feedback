1e4489f0cd6d5941d17ca42066924a12f2e4887e
Michael <michael.kononenko101@gmail.com>
2022-12-11 15:18:36 +0200

    bug fix

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 
 export const Notification = ({message}) => (
 <div style={{fontSize:"20px", border: "1px dashed black", borderRadius:"10px", padding: "10px", textAlign:"center"}}>{message}</div>)
